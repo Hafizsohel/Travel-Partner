a90967c8fa10d16390de53d428b611f408f982e8
Hafizur Rahman <sohel.hafizurrahman@gmail.com>
2023-11-27 21:20:45 +0600

    silder and nav design

--- app/src/main/java/com/example/travelpartner/DashboardFragment.java
+++ app/src/main/java/com/example/travelpartner/DashboardFragment.java
@@ -1,20 +1,146 @@
 package com.example.travelpartner;
 
+import android.animation.ObjectAnimator;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.travelpartner.Adapter.ImageSliderAdapter;
+import com.example.travelpartner.databinding.FragmentDashboardBinding;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DashboardFragment extends Fragment {
+    FragmentDashboardBinding binding;
+    private ImageSliderAdapter slideAdapter;
+    private DrawerLayout drawerLayout;
+    private ImageView[] indicators;
+    private ViewPager viewPager;
+    private int currentPage = 0;
+    private Timer timer;
+    final long DELAY_MS = 1000;
+    final long PERIOD_MS = 3000;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        binding = FragmentDashboardBinding.inflate(getLayoutInflater());
+
+
+        viewPager = binding.viewPager;
+        int[] slideImages = {R.drawable.banner1, R.drawable.banner2, R.drawable.banner3, R.drawable.banner4, R.drawable.banner5};
+
+        slideAdapter = new ImageSliderAdapter(requireContext(), slideImages);
+        viewPager.setAdapter(slideAdapter);
+
+        setupIndicator();
+        startAutoSlide();
+        drawerLayout=binding.drawerLayer;
+
+        binding.btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrawer(drawerLayout);
+            }
+        });
+
+
+        return binding.getRoot();
+    }
+
+    private void setupIndicator() {
+        if (viewPager != null && viewPager.getAdapter() != null) {
+            int slideCount = viewPager.getAdapter().getCount();
+            indicators = new ImageView[slideCount];
+
+            for (int i = 0; i < slideCount; i++) {
+                indicators[i] = new ImageView(requireContext());
+                indicators[i].setImageResource(R.drawable.unselected_indicator); // Set the unselected indicator drawable
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+                params.setMargins(8, 0, 8, 0); // Set margins between indicators
+                binding.indicatorLayout.addView(indicators[i], params);
+            }
+
+            setCurrentIndicator(0);
+
+            viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+                @Override
+                public void onPageSelected(int position) {
+                    setCurrentIndicator(position);
+                    currentPage = position;
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {}
+            });
+        } else {
+        }
+    }
+
+    private void setCurrentIndicator(int position) {
+        if (indicators != null) {
+            for (int i = 0; i < indicators.length; i++) {
+                if (i == position) {
+                    indicators[i].setImageResource(R.drawable.selected_indicator); // Set the selected indicator drawable
+                } else {
+                    indicators[i].setImageResource(R.drawable.unselected_indicator); // Set the unselected indicator drawable
+                }
+            }
+        }
+    }
+
+    private void startAutoSlide() {
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (currentPage == indicators.length) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_MS, PERIOD_MS);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+    private void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 }
+
+
--- app/src/main/java/com/example/travelpartner/MainActivity.java
+++ app/src/main/java/com/example/travelpartner/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.travelpartner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.travelpartner.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.FrameLayoutID, new DashboardFragment()).commit();
+
+        }
     }
 }
