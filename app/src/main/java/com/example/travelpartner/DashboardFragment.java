02ed43f34bd5ca5ea99969f598ce73515d113e6e
Hafizur Rahman <sohel.hafizurrahman@gmail.com>
2023-12-13 17:22:23 +0600

    3rd design update

--- app/src/main/java/com/example/travelpartner/DashboardFragment.java
+++ app/src/main/java/com/example/travelpartner/DashboardFragment.java
@@ -1,28 +1,18 @@
 package com.example.travelpartner;
 
-import android.animation.ObjectAnimator;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
-
 import com.example.travelpartner.Adapter.ImageSliderAdapter;
 import com.example.travelpartner.databinding.FragmentDashboardBinding;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -31,7 +21,7 @@ public class DashboardFragment extends Fragment {
     private ImageSliderAdapter slideAdapter;
     private DrawerLayout drawerLayout;
     private ImageView[] indicators;
-    private ViewPager viewPager;
+    //private ViewPager viewPager;
     private int currentPage = 0;
     private Timer timer;
     final long DELAY_MS = 1000;
@@ -43,11 +33,11 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(getLayoutInflater());
 
 
-        viewPager = binding.viewPager;
+       // viewPager = binding.viewPager;
         int[] slideImages = {R.drawable.banner1, R.drawable.banner2, R.drawable.banner3, R.drawable.banner4, R.drawable.banner5};
 
         slideAdapter = new ImageSliderAdapter(requireContext(), slideImages);
-        viewPager.setAdapter(slideAdapter);
+        binding.viewPager.setAdapter(slideAdapter);
 
         setupIndicator();
         startAutoSlide();
@@ -63,24 +53,23 @@ public class DashboardFragment extends Fragment {
     }
 
     private void setupIndicator() {
-        if (viewPager != null && viewPager.getAdapter() != null) {
-            int slideCount = viewPager.getAdapter().getCount();
+        if (binding.viewPager != null && binding.viewPager.getAdapter() != null) {
+            int slideCount = binding.viewPager.getAdapter().getCount();
             indicators = new ImageView[slideCount];
 
             for (int i = 0; i < slideCount; i++) {
                 indicators[i] = new ImageView(requireContext());
-                indicators[i].setImageResource(R.drawable.unselected_indicator); // Set the unselected indicator drawable
+                indicators[i].setImageResource(R.drawable.unselected_indicator);
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.WRAP_CONTENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT
                 );
-                params.setMargins(8, 0, 8, 0); // Set margins between indicators
+                params.setMargins(8, 0, 8, 0);
                 binding.indicatorLayout.addView(indicators[i], params);
             }
-
             setCurrentIndicator(0);
 
-            viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            binding.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
                 @Override
                 public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
 
@@ -101,9 +90,9 @@ public class DashboardFragment extends Fragment {
         if (indicators != null) {
             for (int i = 0; i < indicators.length; i++) {
                 if (i == position) {
-                    indicators[i].setImageResource(R.drawable.selected_indicator); // Set the selected indicator drawable
+                    indicators[i].setImageResource(R.drawable.selected_indicator);
                 } else {
-                    indicators[i].setImageResource(R.drawable.unselected_indicator); // Set the unselected indicator drawable
+                    indicators[i].setImageResource(R.drawable.unselected_indicator);
                 }
             }
         }
@@ -116,7 +105,7 @@ public class DashboardFragment extends Fragment {
                 if (currentPage == indicators.length) {
                     currentPage = 0;
                 }
-                viewPager.setCurrentItem(currentPage++, true);
+                binding.viewPager.setCurrentItem(currentPage++, true);
             }
         };
 
